124ea8eedcea4e143cdaf1275a6a3b4873b5cece
G'ayrat <shirnboyevgayrat@gmail.com>
2022-08-01 13:40:02 +0500

    dgdf2

--- bin/basic_class_homework02.dart
+++ bin/basic_class_homework02.dart
@@ -2,5 +2,11 @@
 Create a "Person" class. 
 Create an attribute "name" in the "Person" class.
 */
+class Person {
+  String name = 'Gayrat';
+}
 
-void main() {}
+void main() {
+  Person x = Person();
+  print(x.name);
+}
