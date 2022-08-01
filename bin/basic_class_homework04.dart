8498679c92e9d5b89b2a9b89af61c6b0270372f9
G'ayrat <shirnboyevgayrat@gmail.com>
2022-08-01 14:36:52 +0500

    dgdf4

--- bin/basic_class_homework04.dart
+++ bin/basic_class_homework04.dart
@@ -6,4 +6,19 @@ Create an attribute "name" in the "Person" class.
 create an object named "p1" whose name is "Anvar" 
 create an object named "p2" whose name is "Shavkat"
 */
-void main() {}
+class Person {
+  String pi1 = 'Anvar';
+  String pi2 = 'Shavakt';
+  Person({String pi1 = 'Anvar', String pi2 = 'Shavkat'}) {
+    this.pi1 = pi1;
+    this.pi2 = pi2;
+  }
+  String getInfo() {
+    return 'pi1:${this.pi1}pi2:${this.pi2}';
+  }
+}
+
+void main() {
+  Person x = Person(pi1: ' Anvar ', pi2: ' Shavkat');
+  print(x.getInfo());
+}
