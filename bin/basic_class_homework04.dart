ffcf1e1ccd5bd650ae6e4b9b14aae95c6e950332
G'ayrat <shirnboyevgayrat@gmail.com>
2022-08-01 14:47:42 +0500

    dsfad

--- bin/basic_class_homework04.dart
+++ bin/basic_class_homework04.dart
@@ -7,18 +7,15 @@ create an object named "p1" whose name is "Anvar"
 create an object named "p2" whose name is "Shavkat"
 */
 class Person {
-  String pi1 = 'Anvar';
-  String pi2 = 'Shavakt';
-  Person({String pi1 = 'Anvar', String pi2 = 'Shavkat'}) {
-    this.pi1 = pi1;
-    this.pi2 = pi2;
-  }
-  String getInfo() {
-    return 'pi1:${this.pi1}pi2:${this.pi2}';
+  String name = '';
+  Person(name) {
+    this.name = name;
   }
 }
 
 void main() {
-  Person x = Person(pi1: ' Anvar ', pi2: ' Shavkat');
-  print(x.getInfo());
+  Person p1 = Person('Anvar');
+  Person p2 = Person('Shavkat');
+  print(p1.name);
+  print(p2.name);
 }
